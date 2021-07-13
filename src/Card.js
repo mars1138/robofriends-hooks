2b359465685fb397819d1f8ebbf13e212fd0de4e
Mario Delgado <mdelgado1138@hotmail.com>
2021-07-13 16:38:35 -0700

    initial

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// destructure props right away, without having to do declarations inside function body
+const Card = ({ name, email, id }) => {
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
